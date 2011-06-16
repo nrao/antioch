fa50ca26a5313f96181e6f45d3611c2ec612ec30
Paul Marganian <pmargani@nrao.edu>
2011-06-16 12:02:33 -0400

    Added boilerPlate to all files for copyright reasons.

--- admin/Sun.py
+++ admin/Sun.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python 
 # -*- coding: iso-8859-1 -*-
+
+# Copyright (C) 2009 Associated Universities, Inc. Washington DC, USA.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 675 Mass Ave Cambridge, MA 02139, USA.
+#
+# Correspondence concerning GBT software should be addressed as follows:
+#     GBT Operations
+#     National Radio Astronomy Observatory
+#     P. O. Box 2
+#     Green Bank, WV 24944-0002 USA
+
 """ 
 SUNRISET.C - computes Sun rise/set times, start/end of twilight, and
              the length of the day at any date and latitude
